3ea398905d062800a1ba2b7d03f28ef728e56938
underwood.brady <underwood.brady@gmail.com>
2024-12-03 12:41:20 -0800

    Initial commit pt.2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+    import Icon from "virtual:icons/mdi/store-24-hour";
+</script>
+
+<Icon/>
+<h1 class="bg-sage-12 text-teal-1">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
