bede76c0337c03b9bba16818d58806af9fb0a956
underwood.brady <underwood.brady@gmail.com>
2024-12-03 16:31:13 -0800

    nice animation !

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -64,9 +64,34 @@
 	];
 
 	const user = 'User'; //TODO: Fill in
+
+	let activeItemHeight = 0;
+	let activeItemTop = 0;
+
+	function activeIndicator(node: HTMLElement, isActive: boolean) {
+		if (isActive) {
+			activeItemHeight = node.offsetHeight;
+			activeItemTop = node.offsetTop;
+		}
+
+		return {
+			update(newIsActive: boolean) {
+				if (newIsActive) {
+					activeItemHeight = node.offsetHeight;
+					activeItemTop = node.offsetTop;
+				}
+			}
+		};
+	}
 </script>
 
 <aside class="fixed left-0 top-0 h-screen w-56 bg-sagedark-3 pt-xl flex flex-col justify-between">
+	{#if activeItemHeight && activeItemTop}
+		<div
+			class="absolute z-20 top-0 right-0 w-xs bg-teal-9 transition-all duration-300 ease-in-out rounded-r-md indicator"
+			style="height: {activeItemHeight}px; transform: translateY({activeItemTop}px);"
+		></div>
+	{/if}
 	<div>
 		<h1 class="px-lg text-xl font-semibold mb-lg">Title</h1>
 		<ul class="text-sm space-y-sm text-sagedark-11 font-medium">
@@ -77,15 +102,12 @@
 					<li>
 						<a
 							href={page.route}
-							class='relative flex items-center space-x-sm px-lg py-sm hover:bg-sagedark-4 active:bg-sagedark-5 transition-colors duration-100 rounded-sm {page.route == currentPage && 'bg-sagedark-5 text-sagedark-12'}'
+							class="relative flex items-center space-x-sm px-lg py-sm hover:bg-sagedark-4 active:bg-sagedark-5 transition-colors duration-100 rounded-sm {page.route ==
+								currentPage && 'bg-sagedark-5 text-sagedark-12'}"
+							use:activeIndicator={page.route === currentPage}
 						>
 							<svelte:component this={page.icon} class="w-4 h-4" />
 							<span>{page.page}</span>
-                            {#if page.route == currentPage}
-                                <div class="absolute right-0 top-0 h-full w-xs rounded-r-md bg-teal-9">
-
-                                </div>
-                            {/if}
 						</a>
 					</li>
 				{/if}
@@ -102,3 +124,9 @@
 		>
 	</div>
 </aside>
+
+<style>
+	.indicator {
+		will-change: transform;
+	}
+</style>
